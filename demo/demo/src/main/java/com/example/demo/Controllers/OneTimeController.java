0792c08e3fb5be1bada8f4b9a7b8d8f27352ab02
Inderneel08 <68139492+Inderneel08@users.noreply.github.com>
2024-09-03 23:21:43 +0530

    09/03/2024

--- demo/demo/src/main/java/com/example/demo/Controllers/OneTimeController.java
+++ demo/demo/src/main/java/com/example/demo/Controllers/OneTimeController.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.ServiceLayer.HistoryServiceLayer;
 import com.example.demo.ServiceLayer.OneTimeServiceLayer;
 
 @CrossOrigin(origins = "http://localhost:3000/")
@@ -18,6 +20,9 @@ public class OneTimeController {
     @Autowired
     private OneTimeServiceLayer oneTimeServiceLayer;
 
+    @Autowired
+    private HistoryServiceLayer historyServiceLayer;
+
     @GetMapping("/getAllStates")
     public ResponseEntity<?> getStates() {
 
@@ -25,7 +30,14 @@ public class OneTimeController {
 
         responseData.put("states", oneTimeServiceLayer.getAllStates());
 
-        return(ResponseEntity.ok().body(responseData));
+        return (ResponseEntity.ok().body(responseData));
+    }
+
+    @GetMapping("/fetchOrderHistory")
+    public ResponseEntity<?> fetchOrderHistory(@RequestBody Map<String, Object> request) {
+        String email = (String) request.get("email");
+
+        return (ResponseEntity.ok().build());
     }
 
 }
---
+++ demo/demo/src/main/java/com/example/demo/ServiceLayer/HistoryServiceLayer.java
@@ -0,0 +1,11 @@
+package com.example.demo.ServiceLayer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HistoryServiceLayer {
+
+    public void findHistoryByEmail(String email) {
+
+    }
+}
